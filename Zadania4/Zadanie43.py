0780270085782a0c4a867c028a4bb1a9c5bc26d4
Przemyslaw Sagalo <przemyslaw.sagalo@uj.edu.pl>
2016-10-25 12:34:09 +0200

    43 done

--- Zadania4/Zadanie43.py
+++ Zadania4/Zadanie43.py
@@ -1,2 +1,19 @@
 # Python 2.7.4
 
+import math
+
+def Mfactorial(n):
+	val_factorial = 1
+	if n > 1:
+		i = 2
+		while i <= n:
+			val_factorial *= i
+			i += 1
+	elif n <= 1:
+		val_factorial = 1
+	
+	return val_factorial 
+
+# for i in xrange(10):
+# 	if Mfactorial(i) == math.factorial(i):
+# 		print i, 'OK'
