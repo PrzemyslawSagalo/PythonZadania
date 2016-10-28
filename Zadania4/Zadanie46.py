ebfb6a8337f0fb9d6e6a1410bd550ccbf9abccac
Przemyslaw Sagalo <przemyslaw.sagalo@uj.edu.pl>
2016-10-28 17:57:17 +0200

    46 done

--- Zadania4/Zadanie46.py
+++ Zadania4/Zadanie46.py
@@ -1,3 +1,16 @@
 # Python 2.7.4
 
+def sum_seq(seq):
+	val_sum = 0
+	for el_seq in seq:
+		if isinstance(el_seq, (list, tuple)):
+			val_sum += sum(el_seq)
+		else:
+			val_sum += el_seq
 
+	return val_sum
+
+
+test_seq = [1,(2,3),[],[3,4]]
+
+print sum_seq(test_seq)
