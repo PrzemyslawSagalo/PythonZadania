862b61f01421949bcf2eb6bea5734606f6e347d6
Przemyslaw Sagalo <przemyslaw.sagalo@uj.edu.pl>
2016-11-02 22:01:45 +0100

    47 done

--- Zadania4/Zadanie47.py
+++ Zadania4/Zadanie47.py
@@ -1,17 +1,14 @@
 # Python 2.7.4
 
-fresh_seq = [1,(2,3),[],[4,(5,6,7)],8,[9],(10,11,(12,13,(14)))]
+test_seq = [1,(2,3),[],[4,(5,6,7)],8,[9]]
 
-total_seq = []
 def flatten(seq):
+	if isinstance(seq, (list,tuple)):
+		if len(seq) == 0:
+			return []
+		first, second = seq[0], seq[1:]
+		return flatten(first) + flatten(second)
+	else:
+		return [seq]
 
-
-	for item in seq:
-		if isinstance(item, (tuple, list)):
-			item = flatten(item)
-		else:
-			total_seq.append(item)
-
-	return total_seq
-
-print flatten(fresh_seq)
+print flatten(test_seq)
