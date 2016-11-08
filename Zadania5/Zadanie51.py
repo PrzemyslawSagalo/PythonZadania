8714dc48cc5cd91b606198b312672f1054f1e913
Przemyslaw Sagalo <przemyslaw.sagalo@gmail.com>
2016-11-08 13:03:34 +0100

    51 done

--- Zadania5/Zadanie51.py
+++ Zadania5/Zadanie51.py
@@ -1,1 +1,11 @@
 # Python 2.7.4
+
+import rekurencja as rek
+
+print rek.Mfactorial(6)
+print rek.fibonacci(5)
+
+reload(rek)
+
+print rek.Mfactorial(6)
+print rek.fibonacci(5)
---
+++ Zadania5/rekurencja.py
@@ -0,0 +1,23 @@
+# Python 2.7.4
+
+def fibonacci(n):
+    """Ciag Fibonacciego (definicja rekurencyjna)."""
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+	return l_fib[n]
+
+
+def Mfactorial(n):
+	val_factorial = 1
+	if n > 1:
+		i = 2
+		while i <= n:
+			val_factorial *= i
+			i += 1
+	elif n <= 1:
+		val_factorial = 1
+	
+	return val_factorial 
