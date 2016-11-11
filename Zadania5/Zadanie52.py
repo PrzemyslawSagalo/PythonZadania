9cad888bf7521f885c5237fa9e8aa6fdd4c3a84c
Przemyslaw Sagalo <przemyslaw.sagalo@gmail.com>
2016-11-11 21:20:11 +0100

    52 done

--- Zadania5/Zadanie52.py
+++ Zadania5/Zadanie52.py
@@ -1,1 +1,40 @@
 # Python 2.7.4
+
+import unittest
+from fracs import *
+
+class TestFractions(unittest.TestCase):
+
+	def setUp(self):
+		self.zero = [0, 1]
+
+	def test_add_frac(self):
+		self.assertEqual(add_frac([1, 2], [1, 3]), [5, 6])
+
+	def test_sub_frac(self):
+		self.assertEqual(sub_frac([1, 2], [1, 3]), [1, 6])
+
+	def test_mul_frac(self):
+		self.assertEqual(mul_frac([1, 2], [1, 3]), [1, 6])
+
+	def test_div_frac(self):
+		self.assertEqual(div_frac([1, 2], [1, 3]), [3, 2])
+
+	def test_is_positive(self):
+		self.assertEqual(is_positive([1,2]), 1)
+		self.assertEqual(is_positive([-1,2]), 0)
+
+	def test_is_zero(self):
+		self.assertEqual(is_zero([0,2]), 1)
+		self.assertEqual(is_zero([1,2]), 0)
+
+	def test_cmp_frac(self):
+		self.assertEqual(cmp_frac([1,1], [1,2]), 1)
+		self.assertEqual(cmp_frac([1,2], [1,2]), 0)
+		self.assertEqual(cmp_frac([1,2], [1,1]), -1)
+
+	def test_frac2float(self):
+		self.assertEqual(frac2float([1,2]), 0.5)
+
+if __name__ == '__main__':
+	unittest.main()     # uruchamia wszystkie testy
---
+++ Zadania5/fracs.py
@@ -0,0 +1,126 @@
+# Python 2.7.4
+
+def GCD(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+def fractional_form(a, b):
+	if b != 1:
+		print a, '/' ,b
+	elif b == 1:
+		print a 
+
+def add_frac(frac1, frac2):
+	"""
+	 frac1 + frac2
+	"""
+	if frac1[1] == 0 or frac2[1] == 0:
+		raise ZeroDivisionError
+	else:
+		result = [(frac1[0]*frac2[1]) + (frac1[1]*frac2[0]), frac1[1]*frac2[1]]
+		val_GCD = GCD(*result)
+		result = [result[0]/val_GCD, result[1]/val_GCD]
+
+	return result
+
+def sub_frac(frac1, frac2):
+	"""
+	 frac1 - frac2
+	"""
+	if frac1[1] == 0 or frac2[1] == 0:
+		raise ZeroDivisionError
+	else:
+		result = [(frac1[0]*frac2[1]) - (frac1[1]*frac2[0]), frac1[1]*frac2[1]]
+		val_GCD = GCD(*result)
+		result = [result[0]/val_GCD, result[1]/val_GCD]
+
+	return result
+
+def mul_frac(frac1, frac2):
+	"""
+	 frac1 * frac2
+	"""
+	if frac1[1] == 0 or frac2[1] == 0:
+		raise ZeroDivisionError
+	else:
+		result = [frac1[0] * frac2[0], frac1[1] * frac2[1]]
+		val_GCD = GCD(*result)
+		result = [result[0]/val_GCD, result[1]/val_GCD]
+
+	return result
+
+def div_frac(frac1, frac2):
+	"""
+	 frac1 / frac2
+	"""
+	if frac1[1] == 0 or frac2[1] == 0:
+		raise ZeroDivisionError
+	else:
+		result = [frac1[0] * frac2[1], frac1[1] * frac2[0]]
+		val_GCD = GCD(*result)
+		result = [result[0]/val_GCD, result[1]/val_GCD]
+
+	return result
+
+def is_positive(frac):
+	"""
+	bool, czy dodatni
+	"""
+	is_positive = 0
+
+	if frac[1] == 0:
+		raise ZeroDivisionError
+	else:
+		if frac[0] >= 0 and frac[1] > 0:
+			is_positive = 1
+
+	return is_positive 
+
+
+def is_zero(frac):
+	"""
+	bool, typu [0, x]
+	"""
+	is_num_eq_zero = 0
+
+	if frac[1] == 0:
+		raise ZeroDivisionError
+	else:
+		if frac[0] == 0:
+			is_num_eq_zero = 1
+
+	return is_num_eq_zero
+
+def cmp_frac(frac1, frac2):
+	"""
+	-1 | 0 | +1
+	"""
+	which_greater = 0
+
+	if frac1[1] == 0 or frac2[1] == 0:
+		raise ZeroDivisionError
+	else:
+		frac1 = frac1[0] / frac1[1]
+		frac2 = frac2[0] / frac2[1]
+
+		if 	frac1 > frac2:
+			which_greater = 1
+		elif frac1 == frac2:
+			which_greater = 0
+		elif frac1 < frac2:
+			which_greater = -1
+
+	return which_greater
+
+def frac2float(frac):
+	"""
+	conversion to float
+	"""
+
+	if frac[1] == 0:
+		raise ZeroDivisionError
+	else:
+		result = float(frac[0]) / float(frac[1])
+
+	return result
