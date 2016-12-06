a98ac794dc4bc5afbf882161de07c6b087be4fb8
Przemyslaw Sagalo <przemyslaw.sagalo@uj.edu.pl>
2016-12-06 13:05:18 +0100

    new version of condition in 84

--- Zadania8/Zadanie84/Zadanie84.py
+++ Zadania8/Zadanie84/Zadanie84.py
@@ -3,9 +3,11 @@ import math
 def heron(a, b, c):
     """Obliczanie pola powierzchni trojkata za pomoca wzoru
     Herona. Dlugosci bokow trojkata wynosza a, b, c."""
-    if a + b < c:
-        raise ValueError
     p = 0.5 * (a + b + c)
-    S = math.sqrt(p * (p - a) * (p - b) * (p - c))
+
+    if a < b + c and  b < a + c and c < a + b and 2 * a < 2 * p and 2 * b < 2 * p and 2 * c < 2 * p and p - a > 0 and p - b > 0 and p - c > 0:
+        S = math.sqrt(p * (p - a) * (p - b) * (p - c))
+    else:
+        raise ValueError
 
     return S
