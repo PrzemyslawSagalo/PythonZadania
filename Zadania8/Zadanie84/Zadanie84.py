e82a250e29785eece02907797a5998eac93f2d87
Przemyslaw Sagalo <przemyslaw.sagalo@uj.edu.pl>
2017-01-03 21:06:16 +0100

    ex 84 has been repaired

--- Zadania8/Zadanie84/Zadanie84.py
+++ Zadania8/Zadanie84/Zadanie84.py
@@ -5,7 +5,7 @@ def heron(a, b, c):
     Herona. Dlugosci bokow trojkata wynosza a, b, c."""
     p = 0.5 * (a + b + c)
 
-    if a < b + c and  b < a + c and c < a + b and 2 * a < 2 * p and 2 * b < 2 * p and 2 * c < 2 * p and p - a > 0 and p - b > 0 and p - c > 0:
+    if a < b + c and  b < a + c and c < a + b:
         S = math.sqrt(p * (p - a) * (p - b) * (p - c))
     else:
         raise ValueError('Podane wartosci nie tworza dlugosci bokow trojkata')
