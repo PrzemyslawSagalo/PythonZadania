25ed652f641151f18e59d90f1911f41ee294e56b
Przemyslaw Sagalo <przemyslaw.sagalo@uj.edu.pl>
2017-02-14 10:27:36 +0100

    Zadanie 102 has been done

---
+++ Zadania10/Zadanie102/testy102.py
@@ -0,0 +1,33 @@
+# Python 2.7.4
+
+import unittest
+import Zadanie102
+
+class TestCircles(unittest.TestCase):
+
+    def setUp(self):
+        self.stos_testowy1 = Zadanie102.Stack()
+        self.stos_testowy2 = Zadanie102.Stack()
+
+    def test_przepelnienie(self):
+        '''
+        test przepelnienia stosu
+        '''
+        with self.assertRaises(ValueError):
+            for i in range(11):
+                self.stos_testowy1.push(i)
+
+    def test_pobieranie_pusty(self):
+        '''
+        test pobierania z pustego stosu
+        '''
+        with self.assertRaises(ValueError):
+            self.stos_testowy2.pop()
+
+
+
+    def tearDown(self):
+        self.stos_testowy1 = None
+
+if __name__ == "__main__":
+    unittest.main()     # wszystkie testy
