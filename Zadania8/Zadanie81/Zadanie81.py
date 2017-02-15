b914a551e3ad930c292e556ae589c4270cbff2b5
Przemyslaw Sagalo <przemyslaw.sagalo@uj.edu.pl>
2017-02-15 22:36:39 +0100

    81 has been rapaired

--- Zadania8/Zadanie81/Zadanie81.py
+++ Zadania8/Zadanie81/Zadanie81.py
@@ -1,15 +1,20 @@
 def solve1(a, b, c):
     """Rozwiazywanie rownania liniowego a x + b y + c = 0."""
-    if b == 0:
-        raise ValueError('b nie moze byc rowne 0')
-    else:
-        if a == 0 and c == 0:
-            to_return = 'y=0'
-        elif a == 0:
-            to_return = 'y={}'.format(-c / float(b))
-        elif c == 0:
-            to_return = 'y={}*x'.format(-a/float(b))
-        else:
-            to_return = 'y={0}*x{1}'.format(-a/float(b), -c/float(b))
+    if a == 0 and b == 0 and c == 0:
+        to_return = 'rownanie jest zawsze spelnione'
+    elif a == 0 and b == 0 and c != 0:
+        to_return = 'rownanie jest sprzeczne'
+    elif a == 0 and b != 0 and c == 0:
+        to_return = 'y=0'
+    elif a == 0 and b != 0 and c != 0:
+        to_return = 'y={}'.format(-c/float(b))
+    elif a != 0 and b == 0 and c == 0:
+        to_return = 'x=0'
+    elif a != 0 and b == 0 and c != 0:
+        to_return = 'x={}'.format(-c/float(a))
+    elif a != 0 and b != 0 and c == 0:
+        to_return = 'y={} x'.format(-a/float(b))
+    elif a != 0 and b != 0 and c != 0:
+        to_return = 'y={0} x {1}'.format(-a/float(b), -c/float(b))
 
     return to_return
